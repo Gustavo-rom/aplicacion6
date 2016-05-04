4866c1ada26b2d3477f25bc6a63f4bfd105418f0
Gustavo-rom <Gustavo.dsaupoo@gmail.com>
2016-05-04 00:22:10 -0500

    4

--- js/acciones.js
+++ js/acciones.js
@@ -1,15 +1,11 @@
-// JavaScript 
-
+// JavaScript Document
 $(document).ready(function(e) {
-	document .addEventListener("deviceready",function(){
-		
-		$('#beep').tap(function (){
-			navigator .notification.beep (1);
-		});//tap beep
-		
-		$('#vibrar').tap(function(){
-				navigator .notification.vibrate(1000);
-		});//tap vibrar
-		
-	},false);//deviceready
-});//ready
+document.addEventListener ("deviceready",function(){
+ $('#beep').tap(function(){
+	 navigator.notification.beep (1);	 
+});
+ $('#vibrar').tap(function(){
+	 navigator.notification.vibrate (1);	 
+});
+},false);
+});
